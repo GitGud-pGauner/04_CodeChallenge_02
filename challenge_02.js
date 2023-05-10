c15d55a2807ac8275b94ae4c858c74a06f394111
Ozan Özyurt <132558062+pGauner@users.noreply.github.com>
2023-05-10 14:39:47 +0200

    final

--- challenge_02.js
+++ challenge_02.js
@@ -17,25 +17,55 @@ Alter 13-17 // Cola
 /* Getränke-Challenge */
 
 
+// const name = prompt("Bitte geben Sie Ihren Namen ein:");
+
+// // Prompt für Alterseingabe
+// const age = parseInt(prompt("Bitte geben Sie Ihr Alter ein:"));
+
+// // Alterseingabe und Getränkeoption
+// let drink;
+
+// if (age >= 0 == age <= 5) {
+//   drink = "Milch";
+// } else if (age >= 6 == age <= 12) {
+//   drink = "Saft";
+// } else if (age >= 13 == age <= 17) {
+//   drink = "Cola";
+// } else if (age >= 18) {
+//   drink = "Wein";
+// } else {
+//   drink = "Ungültiges Alter";
+// }
+
+// // Zeige Getränkewahl an
+// console.log(drink + " =)");
+
+
+
+//Switch|Case
+
+//Namen- und Alterseingabe
 const name = prompt("Bitte geben Sie Ihren Namen ein:");
-
-// Prompt für Alterseingabe
 const age = parseInt(prompt("Bitte geben Sie Ihr Alter ein:"));
-
-// Alterseingabe und Getränkeoption
 let drink;
 
-if (age >= 0 == age <= 5) {
-  drink = "Milch";
-} else if (age >= 6 == age <= 12) {
-  drink = "Saft";
-} else if (age >= 13 == age <= 17) {
-  drink = "Cola";
-} else if (age >= 18) {
-  drink = "Wein";
-} else {
-  drink = "Ungültiges Alter";
+switch (true) {
+  case (age >= 0 == age <= 5):
+    drink = "Milch";
+    break;
+  case (age >= 6 == age <= 12):
+    drink = "Saft";
+    break;
+  case (age >= 13 == age <= 17):
+    drink = "Cola";
+    break;
+  case (age >= 18):
+    drink = "Wein";
+    break;
+  default:
+    drink = "Ungültiges Alter";
+    break;
 }
 
-// Getränkewahl anzeigen
-console.log(name + " trinkt " + drink + ".");
+// Zeige Getränkewahl an
+console.log(drink + " =)");
